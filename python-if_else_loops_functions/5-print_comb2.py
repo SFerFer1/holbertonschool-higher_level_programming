6d332c191b17a3ae84efacc825db76e7d85b69e9
SFerFer1 <10048@holbertonstudents.com>
2025-01-15 10:08:04 -0300

    5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 for i in range(100):
-    print("{:02}, ".format(i), end ="")
+    if i is not 99:
+        print("{:02}, ".format(i), end ="")
+    else:
+        print("{:02}".format(i), end ="")
 print("")
