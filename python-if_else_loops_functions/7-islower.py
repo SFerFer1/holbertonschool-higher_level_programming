d24170045b324c44ef02467f5a1f3fbbe0dc7b27
SFerFer1 <10048@holbertonstudents.com>
2025-01-15 11:33:29 -0300

    7

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def islower(c):
     if ord(c)in range(97, 123):
-        print("{} is lower".format(c))
         return True
     elif ord(c)in range(65, 91):
-        print("{} is upper".format(c))
         return False
     else:
         return False
