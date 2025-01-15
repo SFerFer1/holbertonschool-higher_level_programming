8b2e57bd000612f360f345bf9a1d1342f7ccc522
SFerFer1 <10048@holbertonstudents.com>
2025-01-15 11:56:48 -0300

    8

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def islower(c):
-    if ord(c)in range(97, 123):
+    if ord(c) in range(97, 123):
         return True
-    elif ord(c)in range(65, 91):
+    elif ord(c) in range(65, 91):
         return False
     else:
         return False
---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        if ord(c) in range(65, 91):
+            print("{}".format(i+32))
+        else:
+            print("{}".format(i))
+    
