0b41ec88872d7d4172a4ce94ff6e740f48e4be9b
SFerFer1 <10048@holbertonstudents.com>
2025-01-18 21:01:44 +0000

    6

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,11 +2,11 @@
 def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz", end=" ")
+            print("FizzBuzz", end=" " if i < 100 else "")
         elif i % 3 == 0:
-            print("Fizz", end=" ")
+            print("Fizz", end=" " if i < 100 else "")
         elif i % 5 == 0:
-            print("Buzz", end=" ")
-        else:
-            print("{}".format(i), end=" ")
+            print("Buzz", end=" " if i < 100 else "")
+         else:
+            print("{}".format(i), end=" " if i < 100 else "")
     print()
