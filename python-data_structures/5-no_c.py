ae05c75162986027888aa066dd097c743547ef60
SFerFer1 <10048@holbertonstudents.com>
2025-01-20 12:37:13 -0300

    pydcodestyle

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     ret = ""
-    i = 0
-    while i in my_string:
+    for i in my_string:
         if i != "c" and i != "C":
             ret += i
     return ret
