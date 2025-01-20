9ee1218e983c2635aa9d90a80aab8db58f750654
SFerFer1 <10048@holbertonstudents.com>
2025-01-20 10:58:04 -0300

    pydcodestyle

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -3,5 +3,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
     v1, v2 = (tuple_a + (0, 0))[:2]
     v11, v22 = (tuple_b + (0, 0))[:2]
     
-    tuple_c = (v1 + v2, v11 + v22)
-    return tuple_c
+    return (v1 + v2, v11 + v22)
+    
