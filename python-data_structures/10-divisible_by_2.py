c95657023aa8032d8d32517424cae61ea9673b36
SFerFer1 <10048@holbertonstudents.com>
2025-01-20 11:49:07 -0300

    pydcodestyle

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    n = 0
+    for i in my_list:
+        if i%2 != 0:
+            i = i+1
+            print("{} is not divisible by 2".format(i))
+        if i%2 == 0:
+            print("{} is divisible by 2".format(i))
+    if n != 0:
+        return True
+    else: 
+        return False
