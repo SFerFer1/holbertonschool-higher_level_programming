ef70a33e22458024cebe85c523e3f8ebac4e66f5
SFerFer1 <10048@holbertonstudents.com>
2025-01-20 10:34:29 -0300

    pydcodestyle

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    ret[]
-    ret = my_string[:]
+    ret = ""
     i = 0
     while i in my_string:
         if i != "c" and i != "C":
---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    v1, v2 = (tuple_a + (0, 0))[:2]
+    v11, v22 = (tuple_b + (0, 0))[:2]
+    
+    tuple_c = (v1 + v2, v11 + v22)
+    return tuple_c
