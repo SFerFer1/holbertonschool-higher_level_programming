62c08ee5fbc6f6c3a50904228e4d809ffc58f741
SFerFer1 <10048@holbertonstudents.com>
2025-01-20 12:32:17 -0300

    pydcodestyle

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for lista in matrix:
-        print("".join("{}".format(num) for num in lista) + "$")
+        print("".join("{:d}".format(num) for num in lista) + "$")
