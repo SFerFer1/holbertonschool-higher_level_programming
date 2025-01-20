0eb62e9adc77feb7704ba80bc2ad7c9d5b7834ec
SFerFer1 <10048@holbertonstudents.com>
2025-01-20 09:21:12 -0300

    1, 2, 3,

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,4 +2,7 @@
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx > len(my_list):
         return my_list
-    
+    temp = []
+    temp = my_list
+    temp [idx] = element
+    return temp
