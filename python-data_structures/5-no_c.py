e91f052b7f95bef0158035df6c8885f83599f63a
SFerFer1 <10048@holbertonstudents.com>
2025-01-20 09:30:27 -0300

    1, 2, 3,

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
     ret =[]
-    ret = my_string
+    ret = my_string[:]
     i = 0
     while i in my_string:
         if i != "c" and i != "C":
             ret += i
-    return ret    
+    return ret
