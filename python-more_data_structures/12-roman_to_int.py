74a0fea6c6bc349cc904964e81cc22f4fbf7fce0
SFerFer1 <10048@holbertonstudents.com>
2025-01-21 11:32:55 -0300

    ---8

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if  roman_string is not None:
-        return roman_to_int(roman_string)
-    else:
-        return 0
+    print (roman_string)
+    return 0
