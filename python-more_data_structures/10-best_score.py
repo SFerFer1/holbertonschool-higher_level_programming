2af0c5f930e7b0a71f32b8d02260e819c0eda431
SFerFer1 <10048@holbertonstudents.com>
2025-01-21 11:03:35 -0300

    ---8

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    bigger = 0
+    for i in a_dictionary:
+        if a_dictionary[i] > bigger:
+            bigger = a_dictionary[i]
+    
+    return bigger
--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    if a_dictionary is not None:
-        for i in a_dictionary:
-            a_dictionary[i] = (a_dictionary[i] * 2)
+    for i in a_dictionary:
+        a_dictionary[i] = (a_dictionary[i] * 2)
     return a_dictionary
-    
