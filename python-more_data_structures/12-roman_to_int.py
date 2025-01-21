c65aeefa1ad0c8ae6953d3222e03850ede647aba
SFerFer1 <10048@holbertonstudents.com>
2025-01-21 14:32:54 -0300

    ---8

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -33,7 +33,7 @@ def roman_to_int(roman_string):
                 break
             suma += Valores[roman_string[t]]
 
-    
+    print(Valores[letraM])
     total = ((Valores[letraM] - resta) + suma)
    
     return total
