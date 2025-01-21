5464e62ef73fdabec8bf8d70cee02d666aa3790d
SFerFer1 <10048@holbertonstudents.com>
2025-01-21 12:39:14 -0300

    ---8

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,11 +1,26 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    Valores = {'I': 1, 'V': 5,'X': 10,'L': 50}
+    mayor = 0
+    letraM = ""
+    resta = 0
+    suma = 0
     total = 0
+    t = len(roman_string)
     for i in roman_string:
-        if i == 'I':
-            total +=1
-        elif i == 'V':
-            total += 5
-        else:
-            return 0       
-    return total        
+        if Valores[i] > mayor:
+            mayor = Valores[i]
+            letraM = ""
+            letraM += i
+    
+    for n in roman_string:
+        resta += Valores[i]
+        if n == letraM:
+            break
+    
+    while t > 0:
+        suma += Valores[t]
+        if n == letraM:
+            break
+    total = ((Valores[letraM] - resta) + suma)
+    return total
