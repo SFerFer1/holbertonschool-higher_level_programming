08b2dd1927d41cfa78459a50f63affbb82159c64
SFerFer1 <10048@holbertonstudents.com>
2025-01-21 09:01:41 -0300

    ---7

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -2,7 +2,7 @@
 def update_dictionary(a_dictionary, key, value):
     n = 0
     for i in a_dictionary:
-      if i.keys() == key:
+      if i == key:
             a_dictionary[i] = value
             n += 1
     if n  != 0:
