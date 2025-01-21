fbf8e3b0b37d0c8e43ad0d597d7560002ef3ba64
SFerFer1 <10048@holbertonstudents.com>
2025-01-21 09:30:32 -0300

    ---8

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    for i in a_dictionary:
+    for i in range(len(a_dictionary)):
         if i == key:
             a_dictionary.pop(i)
+            return
