9abf549cbff114d570077e4a910427ff1b95d1c9
SFerFer1 <10048@holbertonstudents.com>
2025-01-21 10:57:10 -0300

    ---8

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for i in a_dictionary:
-        a_dictionary[i] = (a_dictionary[i] * 2)
+    if a_dictionary is not None:
+        for i in a_dictionary:
+            a_dictionary[i] = (a_dictionary[i] * 2)
     return a_dictionary
     
