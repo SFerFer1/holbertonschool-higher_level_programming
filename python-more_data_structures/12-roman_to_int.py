424e0a697fe5f7140cf90b7043871726b9bb9b1f
SFerFer1 <10048@holbertonstudents.com>
2025-01-21 11:44:05 -0300

    ---8

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,9 +2,10 @@
 def roman_to_int(roman_string):
     total = 0
     for i in range(len(0, roman_string)):
-        if i == "I":
+        if i == 'I':
             total +=1
-        elif i == "V":
+        elif i == 'V':
             total += 5
-                
+        else:
+            return 0       
     return total        
