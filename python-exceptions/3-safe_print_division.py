84219f5710d69fdf0e146f2d4b25c171d42ca9c5
SFerFer1 <10048@holbertonstudents.com>
2025-01-22 11:39:41 -0300

    ---3

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -5,6 +5,5 @@ def safe_print_division(a, b):
         return total
     except (ZeroDivisionError, TypeError,):
         total = None
-        return None
     finally:
         print("Inside result: {}".format(total))
