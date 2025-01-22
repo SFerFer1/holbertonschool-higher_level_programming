081b2d86266336fd10efbcf958a5f3545d93eb75
SFerFer1 <10048@holbertonstudents.com>
2025-01-22 11:17:28 -0300

    ---3

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -4,9 +4,7 @@ def roman_to_int(roman_string):
     total = 0
     for i in range(len(roman_string)):
         if i + 1 < len(roman_string) and Valores[roman_string[i]] < Valores[roman_string[i + 1]]:
-            total += Valores[roman_string[i]]
-        else:
             total -= Valores[roman_string[i]]
+        else:
+            total += Valores[roman_string[i]]
     return total
-
-    
