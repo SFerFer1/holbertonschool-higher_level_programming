06026df60e16b44d7458f72e53e936900c78daf4
SFerFer1 <10048@holbertonstudents.com>
2025-01-22 08:58:13 -0300

    ---8

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
-def safe_print_list_integers(my_list=[], x=0):
-    
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except TypeError:
+        return False
+
