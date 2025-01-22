403923bae6098c8e6eed5008b13de754772faa23
SFerFer1 <10048@holbertonstudents.com>
2025-01-22 09:08:49 -0300

    ---8

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
+    cont = 0
     for i in range(x):
         try:
-            print("{:d}".format(my_list[i]))
+            print("{:d}".format(my_list[i]), end ="")
+            cont += 1
         except (TypeError, ValueError, IndexError):
             return
-    return
+    return cont
