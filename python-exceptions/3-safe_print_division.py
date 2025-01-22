cbb23b5dbc6cb8a524ce81fa1ec44e65572c58de
SFerFer1 <10048@holbertonstudents.com>
2025-01-22 09:59:16 -0300

    ---3

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -4,9 +4,7 @@ def safe_print_division(a, b):
         total = a/b
         return total
     except:
+        print("{} / {} = None".format(a, b))
         return None
     finally:
-        print("Inside result: {}".format(total))
-
-    
-
+        print("Inside result: {}".format(total))    
