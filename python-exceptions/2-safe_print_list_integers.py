bf63ccf0020f5847a79221bfc7be03d665d0d9c4
SFerFer1 <10048@holbertonstudents.com>
2025-01-22 09:39:28 -0300

    ---8

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -6,7 +6,9 @@ def safe_print_list_integers(my_list=[], x=0):
             print("{:d}".format(my_list[i]), end ="")
             cont += 1
         except (TypeError, ValueError, IndexError) as e:
-            print("Error: {}".format(e))
+            print("Traceback (most recent call last):")
+            print(f"  File \"<stdin>\", line {i+1}, in safe_print_list_integers")
+            print(f"{e.__class__.__name__}: {str(e)}")
             continue
     print()
     return cont
