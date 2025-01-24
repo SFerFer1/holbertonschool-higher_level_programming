a121abc46b6bcf51c6b6cb3f7eae2361a8efc70a
SFerFer1 <10048@holbertonstudents.com>
2025-01-24 23:49:57 +0000

    a

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-"""Este es un módulo que contiene una función para sumar dos números enteros."""
+"""Este es un módulo que contiene una función para sumar d."""
 def add_integer(a, b=98):
     """Suma dos números enteros y devuelve el resultado."""
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    
+
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     a = int(a)
     b = int(b)
+
+
     return a + b
