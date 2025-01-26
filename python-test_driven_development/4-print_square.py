5e246acca75ccdb834a0c44a08972c80cee56226
SFerFer1 <10048@holbertonstudents.com>
2025-01-26 03:15:21 +0000

    s

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Module: 3-say_my_name
+
+This module contains
+a single function that prints the name provided by the user.
+The function ensures
+that both the first name and last name are strings before
+printing the formatted name
+
+Function:
+    - say_my_name:
+      Prints "My name is <first_name> <last_name>" if inputs are valid.
+"""
+
+
+def print_square(size):
+        """
+    Prints a square with the character #.
+
+    Args:
+        size (int): The size length of the square.
+
+    Raises:
+        TypeError: If size is not an integer or
+        if size is a float less than 0.
+        ValueError: If size is less than 0.
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for index in range(size):
+        print("x" * size)
