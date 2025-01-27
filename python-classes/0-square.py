2ae9de97bc62378b4a1b9e32f591b3c4cd0ac85f
SFerFer1 <10048@holbertonstudents.com>
2025-01-27 10:30:26 -0300

    ---0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 class Square:
     """
-    clase Square represante a square.
+    class Square represante a square.
     """
     pass
---
+++ python-classes/2-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+class Square:
+    """
+    clase Square represante a square.
+        Attributes:
+        _size (int or float): The size (length) of one side of the square.
+
+    Methods:
+        __init__(size): Constructor that initializes the size of the square.
+    """
+    def __init__(self, size):
+        """
+        Constructor of class Square.
+        
+        set Square instances
+
+        Parameters:
+            size (int or float): The size of one side of the square.
+        
+        Raise:
+            TypeError: when size is no an integer
+            ValueError: when size is negative or 0 
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self._size = size
