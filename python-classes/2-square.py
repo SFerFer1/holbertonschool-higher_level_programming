aaf67b75c5a0bf57159ccfa70d3e26b61fda18b7
SFerFer1 <10048@holbertonstudents.com>
2025-01-28 09:53:51 -0300

    ---0

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -18,21 +18,21 @@ class Square:
     Methods:
         __init__(size): Constructor that initializes the size of the square.
     """
-    def __init__(self, size):
+    def __init__(self, _Square__size):
         """
         Constructor of class Square.
         
         set Square instances
 
         Parameters:
-            size (int or float): The size of one side of the square.
+           _Square__size  (int or float): The size of one side of the square.
         
         Raise:
             TypeError: when size is no an integer
             ValueError: when size is negative 
         """
-        if not isinstance(size, int):
+        if not isinstance(_Square__size, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        if _Square__size < 0:
             raise ValueError("size must be >= 0")
-        self._Square__size = size
+        self._Square__size = _Square__size
