cd8b7e3993a52344c1d2bc8e95432a2932c88585
SFerFer1 <10048@holbertonstudents.com>
2025-01-28 12:23:33 -0300

    ---1

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+"""
+This module defines a class named Rectangle.
+
+Usage:
+    You can create instances of Rectangle.
+"""
+
+
+class Rectangle:
+    """
+    A class representing a rectangle.
+    """
+    def __init__(self, width=0, height=0):
+
+        """
+        Constructor of class Square.
+
+        set Square instances
+
+        Parameters:
+            width  (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        Raise:
+            TypeError: when size is no an integer
+            ValueError: when size is negative
+        """
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.width = width
+
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.height = height
+
+    @property
+    def width(self):
+        """Retrieve the size of the rectangle."""
+        return self.width
+    
+    @width.setter
+    def width(self, value):
+
+        """Set the width of the square with validation."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.width = value
+
+    @property
+    def height(self):
+        """Retrieve the heigth of the rectangle."""
+        return self.height
+    
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.height = value
+
+    @property
+    def width(self):
+        """Retrieve the width of the rectangle."""
+        return self.width
