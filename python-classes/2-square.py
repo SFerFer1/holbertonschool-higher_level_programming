a1ca6a55fb97a2be4535959acbd8ed68e92e8989
SFerFer1 <10048@holbertonstudents.com>
2025-01-28 10:02:06 -0300

    ---0

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -25,7 +25,7 @@ class Square:
     Methods:
         __init__(size): Constructor that initializes the size of the square.
     """
-    def __init__(self, _Square__size):
+    def __init__(self, _Square__size = 0):
         """
         Constructor of class Square.
         
