c433ba1b3dd88c12ba7bddbce1bac195ef1854e7
SFerFer1 <10048@holbertonstudents.com>
2025-01-28 12:55:39 -0300

    ---1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -31,8 +31,7 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         self.width = width
 
-        if not isinstance(height, int):
-            raise TypeError("height must be an integer")
+
         if height < 0:
             raise ValueError("height must be >= 0")
         self.height = height
@@ -45,8 +44,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """Set the width of the square with validation."""
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
+
         if value < 0:
             raise ValueError("width must be >= 0")
         self.width = value
