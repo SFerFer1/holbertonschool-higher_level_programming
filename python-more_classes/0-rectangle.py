70628d954728b9df452899690bfe0e489661bd19
SFerFer1 <10048@holbertonstudents.com>
2025-01-28 11:48:13 -0300

    ---1

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module defines a class named Rectangle.
+
+For now only do that.
+
+Usage:
+    You can create instances of rectangle.
+"""
+
+
+class Square:
+    """
+    class Square represante a rectangle.
+    """
+    pass
