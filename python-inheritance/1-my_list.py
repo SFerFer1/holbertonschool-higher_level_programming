d6f49b045692807164b9899458b5099ac6d99ef4
SFerFer1 <10048@holbertonstudents.com>
2025-02-05 08:39:07 -0300

    2

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,23 +5,22 @@ This module defines a class named Rectangle.
 Usage:
     You can create instances of Rectangle.
 """
+
+
 class MyList(list):
     """
-    a clase of a list 
+    a clase of a list
     Usage:
     You can create instances of Rectangle.
     """
     def print_sorted(self):
         """
-        print itself sorted 
+        print itself sorted
 
 
         Usage:
         You can create instances of Rectangle.
         """
-        
-
-
-
+ 
         sorted_list = sorted(self)
         print(sorted_list)
