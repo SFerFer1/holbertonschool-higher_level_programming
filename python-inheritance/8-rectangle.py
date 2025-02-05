8bdbedf91939edfd9a72050a9c1220f538217482
SFerFer1 <10048@holbertonstudents.com>
2025-02-05 12:21:49 -0300

    2

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -23,17 +23,17 @@ class Rectangle(BaseGeometry):
         Usage:
         You can create instances of Rectangle.
         """
-        if width <= 0:
-            raise ValueError("width must be greater than 0")
-        
-        if height <= 0:
-            raise ValueError("height must be greater than 0")
-        
         if not isinstance(height, int):
             raise TypeError("height must be an integer")
         
         if not isinstance(width, int):
             raise TypeError("width must be an integer")
+
+        if width <= 0:
+            raise ValueError("width must be greater than 0")
+        
+        if height <= 0:
+            raise ValueError("height must be greater than 0")
 
         self.__width = width
         self.__height = height
