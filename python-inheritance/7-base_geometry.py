0432ca40086756bf01334d3a4530d736f3e05538
SFerFer1 <10048@holbertonstudents.com>
2025-02-05 08:53:49 -0300

    2

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -22,3 +22,5 @@ class BaseGeometry:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
