7be89d5e3032537f5212ea10839bf0126d75ec38
SFerFer1 <10048@holbertonstudents.com>
2025-02-05 13:33:58 -0300

    2

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""
+retun true if is of that class
+
+Usage:
+You can create instances of Rectangle.
+"""
+Rectangle = __import__('11-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    retun true if is of that class
+
+    Usage:
+    You can create instances of Rectangle.
+    """
+    def __init__(self, size):
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size <= 0:
+            raise ValueError("size must be greater than 0")
+
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+    
+    def __str__(self):
+        return (f"[Square] {self.__size}/{self.__size}")
