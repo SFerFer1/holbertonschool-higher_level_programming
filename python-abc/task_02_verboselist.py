c04ea75524e7fcc3c360c97017800f577f320136
SFerFer1 <10048@holbertonstudents.com>
2025-02-05 15:34:53 -0300

    2

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+This module defines a class named Rectangle.
+
+Usage:
+    You can create instances of Rectangle.
+"""
+class VerboseList(list):
+    """
+    This module defines a class named Rectangle.
+
+    Usage:
+    You can create instances of Rectangle.
+    """
+    def append(self, item):
+        super().append(item)
+        print(f"Added {item} to the list.")
+
+    def extend(self, iterable):
+        super().extend(iterable)
+        print(f"Extended the list with {len(iterable)} items.")
+
+    def remove(self, item):
+        print(f"Removed {item} from the list.")
+        super().remove(item)
+
+    def pop(self, index=-1):
+        item = super().pop(index)
+        print(f"Popped {item} from the list.")
+        return item
