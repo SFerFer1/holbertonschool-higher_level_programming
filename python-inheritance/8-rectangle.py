5c2550b5b6b1e90b245655466d0d210af8145fa2
SFerFer1 <10048@holbertonstudents.com>
2025-02-05 12:12:28 -0300

    2

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -18,8 +18,11 @@ class Rectangle(BaseGeometry):
 
     def __init__(self, width, height):
 
-        if width or height > 0:
-            return 0
+        if width > 0:
+            raise ValueError("width must be greater than 0")
+        
+        if height > 0:
+            raise ValueError("height must be greater than 0")
 
         self.__width = width
         self.__height = height
