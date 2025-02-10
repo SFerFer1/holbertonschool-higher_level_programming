ccdc23e23bcd7b006756a9100ba45c9b436439b8
SFerFer1 <10048@holbertonstudents.com>
2025-02-10 09:15:39 -0300

    2

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -6,4 +6,11 @@ Usage:
     You can create instances of Rectangle.
 """
 def write_file(filename="", text=""):
-    
+    """
+    This module defines a class named Rectangle.
+
+    Usage:
+    You can create instances of Rectangle.
+    """
+    with open(filename, 'w', encoding="utf-8") as d:
+        d.write(text)
