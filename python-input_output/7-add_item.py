db315edee6a379a9035a60d54ba01878bb25ea35
SFerFer1 <10048@holbertonstudents.com>
2025-02-10 13:11:48 -0300

    5

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import sys
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
+
+def main():
+
+    try:
+        current_list = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        current_list = []
+
+    current_list.extend(sys.argv[1:])
+
+    save_to_json_file(current_list, "add_item.json")
+
+if __name__ == "__main__":
+    main()
