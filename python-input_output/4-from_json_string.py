cc5724195f1fa1d26ba5bfc1e73249ee7277ba79
SFerFer1 <10048@holbertonstudents.com>
2025-02-12 09:28:00 -0300

    5

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -9,4 +9,10 @@ Usage:
 
 
 def from_json_string(my_str):
+    """
+    This module defines a class named Rectangle.
+
+    Usage:
+    You can create instances of Rectangle.
+    """
     return (json.load(my_str))
