d83ea274a43ed0f479586edfa550a757b817d43d
SFerFer1 <10048@holbertonstudents.com>
2025-02-13 14:56:27 -0300

    w

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+This module defines a class named Rectangle.
+
+Usage:
+    You can create instances of Rectangle.
+"""
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    with open(filename, 'w') as file:
+        json.dump(data, file, indent=4)
+
+data = {'name': 'John', 'age': 30, 'city': 'New York'}
+serialize_and_save_to_file(data, 'output.json')
+
+
+
+def load_and_deserialize(filename):
+    """
+    This module defines a class named Rectangle.
+
+    Usage:
+    You can create instances of Rectangle.
+    """
+    with open(filename, 'r') as file:
+    ret = json.load(file)
+    return ret
+
+
