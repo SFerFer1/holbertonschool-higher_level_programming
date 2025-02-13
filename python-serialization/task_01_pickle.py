eef5866473ed5a8630d09e4f1c31929295395dd7
SFerFer1 <10048@holbertonstudents.com>
2025-02-13 15:25:55 -0300

    w

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -22,6 +22,8 @@ class CustomObject:
 
     def serialize(self, filename):
         try:
+            if os.path.getsize(filename) == 0:
+            raise ValueError("File is empty")
             with open(filename, 'wb') as file:
                 pickle.dump(self, file)
             print(f"Object serialized and saved to {filename}")
