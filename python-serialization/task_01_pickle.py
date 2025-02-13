4718cf5ade755b7876f6ec728a9aed3fdae7b41e
SFerFer1 <10048@holbertonstudents.com>
2025-02-13 15:14:05 -0300

    w

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module defines a class named Rectangle.
+
+Usage:
+    You can create instances of Rectangle.
+"""
+class CustomObject:
+
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        print(f"Name: {self.name}\nAge: {self.age}\nIs Student: {self.is_student}")
