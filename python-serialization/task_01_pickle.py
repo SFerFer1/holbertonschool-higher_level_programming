5a83fba2deaebfb111b34f9f0db34901ccdb7ecd
SFerFer1 <10048@holbertonstudents.com>
2025-02-13 15:23:41 -0300

    w

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -5,6 +5,9 @@ This module defines a class named Rectangle.
 Usage:
     You can create instances of Rectangle.
 """
+import pickle
+
+
 class CustomObject:
 
     def __init__(self, name, age, is_student):
@@ -13,4 +16,23 @@ class CustomObject:
         self.is_student = is_student
 
     def display(self):
-        print(f"Name: {self.name}\nAge: {self.age}\nIs Student: {self.is_student}")
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
+
+    def serialize(self, filename):
+            try:
+            with open(filename, 'wb') as file:
+                pickle.dump(self, file)
+            print(f"Object serialized and saved to {filename}")
+        except Exception as e:
+            print(f"Error occurred while serializing: {e}")
+
+    @classmethod
+    def deserialize(cls, filename: str):
+        try:
+            with open(filename, 'rb') as file:
+                return pickle.load(file)
+        except (FileNotFoundError, pickle.UnpicklingError) as e:
+            print(f"Error occurred while deserializing: {e}")
+            return None
