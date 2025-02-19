e66a1c666785e3c95361146766533a5f04b0ab10
SFerFer1 <10048@holbertonstudents.com>
2025-02-19 15:54:45 -0300

    w

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,6 +1,18 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
+import http.server
+import socketserver
 
-class run(BaseHTTPRequestHandler):
-    
+PORT = 8000
+handler = http.server.SimpleHTTPRequestHandler
+class run(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
-        print("Hello, this is a simple API!")
+        self.send_response(200)
+        
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+
+        self.wfile.write(b"Hello, this is a simple API!")
+
+
+with socketserver.TCPServer(("", PORT),handler ) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
