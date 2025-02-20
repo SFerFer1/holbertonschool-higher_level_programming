74f5941c43b0e72646a8a1033a0a76dd9a6b5130
SFerFer1 <10048@holbertonstudents.com>
2025-02-20 12:44:23 -0300

    3

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -12,7 +12,7 @@ class run(http.server.BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(b"Hello, this is a simple API!")
 
-        if self.path == "/data":
+        elif self.path == "/data":
             data = {
                 "name": "John",
                 "age": 30,
@@ -21,7 +21,15 @@ class run(http.server.BaseHTTPRequestHandler):
             self.send_header("Content-type", "application/json")
             self.end_headers()
             self.wfile.write(json.dumps(data).encode('utf-8'))
-
+        elif self.path == "/status":
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"OK")
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Not Found")
 
 with socketserver.TCPServer(("", PORT),run ) as httpd:
     print("serving at port", PORT)
