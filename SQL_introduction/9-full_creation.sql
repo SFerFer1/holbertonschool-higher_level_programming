b255d4eb47d1c9cc125f97eedf77111a7086de06
SFerFer1 <10048@holbertonstudents.com>
2025-03-05 13:18:15 -0300

    list databases 1st

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -2,10 +2,10 @@
 CREATE TABLE IF NOT EXISTS second_table (
   id INT,
   name VARCHAR(256),
-  socre INT
+  score INT
 );
 
-INSERT INTO second_table (id, name, socre) VALUES
+INSERT INTO second_table (id, name, score) VALUES
 (1, 'John', 10),
 (2, 'Alex', 3),
 (3, 'Bob', 14),
