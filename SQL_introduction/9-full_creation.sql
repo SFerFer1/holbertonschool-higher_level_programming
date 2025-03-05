097bc1ec0606f8ebe19057dfb5616df7d85ef22b
SFerFer1 <10048@holbertonstudents.com>
2025-03-05 13:15:10 -0300

    list databases 1st

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,12 @@
+-- Messi chiquitito
+CREATE TABLE IF NOT EXISTS second_table (
+  id INT,
+  name VARCHAR(256)
+  socre INT
+);
+
+INSERT INTO second_table (id, name, socre) VALUES
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
