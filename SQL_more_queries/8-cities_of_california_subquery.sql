a57e74ac0902a9228ab9f1e8138f7447f3ae777e
SFerFer1 <10048@holbertonstudents.com>
2025-03-06 15:55:30 -0300

    list databases 1st

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -3,5 +3,5 @@
 USE hbtn_0d_usa
 
 SELECT cities.id, cities.state_id, cities.name  FROM cities
-WHERE state_id = (SELECT state_id FROM states WHERE NAME = California)
+WHERE state_id = (SELECT state_id FROM states WHERE NAME = 'California')
 ORDER BY cities.id ASC;
