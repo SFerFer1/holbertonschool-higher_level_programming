a661807a1ea8c81c4fc1bd191625d68eabbe0368
SFerFer1 <10048@holbertonstudents.com>
2025-03-06 16:12:03 -0300

    list databases 1st

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,6 @@
 --asa
 
-SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id
+SELECT cities.id, cities.name, states.name
+FROM cities
+JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
-
