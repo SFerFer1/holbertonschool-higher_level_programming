8ed3d2a2047b4874227de8bb47ea3cf8879fdf74
SFerFer1 <10048@holbertonstudents.com>
2025-03-07 14:08:18 -0300

    list databases 1st

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,7 @@
 -- assa
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-GROUP BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id = NULL
+ORDER BY tv_shows.title ASC;
 
