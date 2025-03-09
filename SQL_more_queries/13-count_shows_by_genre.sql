8af51a63a85bf5946ea74c3625c464a3c4e4f7a0
root <root@LAPTOP-4NQDD1B1.>
2025-03-09 17:58:01 -0300

    sa

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,10 +1,8 @@
 -- aqaa
-USE hbtn_0d_tvshows;
-
 SELECT g.name AS genre, COUNT(s.id) AS number_of_shows
-FROM genres g
-JOIN tv_shows_genres tg ON g.id = tg.genre_id
-JOIN tv_shows s ON tg.tv_show_id = s.id
+FROM tv_genres g
+JOIN tv_shows_genres sg ON g.id = sg.genre_id
+JOIN tv_shows s ON sg.show_id = s.id
 GROUP BY g.name
-HAVING number_of_shows > 0
+HAVING COUNT(s.id) > 0
 ORDER BY number_of_shows DESC;
