526429d14a1f23fc96c3c00f851d1a75f193c7dd
SFerFer1 <10048@holbertonstudents.com>
2025-03-10 14:18:15 -0300

    part 0

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -20,7 +20,8 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states WHERE name"
+    " LIKE %s ORDER BY id ASC", ('N%',))
 
     states = cursor.fetchall()
 
