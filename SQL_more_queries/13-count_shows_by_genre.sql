9984c4645be448706261d554bf0f0a1d46da4038
SFerFer1 <10048@holbertonstudents.com>
2025-03-10 10:24:53 -0300

    asa

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,9 +1,6 @@
 -- assa
-USE hbtn_0d_tvshows
-SELECT g.name AS genre, COUNT(s.id) AS number_of_shows
-FROM tv_genres g
-JOIN tv_shows_genres sg ON g.id = sg.genre_id
-JOIN tv_shows s ON sg.show_id = s.id
-GROUP BY g.name
-HAVING COUNT(s.id) > 0
+SELECT genre, COUNT(*) AS number_of_shows
+FROM shows
+GROUP BY genre
+HAVING number_of_shows > 0
 ORDER BY number_of_shows DESC;
