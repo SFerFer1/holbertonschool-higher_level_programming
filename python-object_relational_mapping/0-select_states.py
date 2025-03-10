e0a0f1a6abe5ae9911234bdd4305db76f12c10d1
SFerFer1 <10048@holbertonstudents.com>
2025-03-10 13:00:43 -0300

    part 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,22 +1,6 @@
 #!/usr/bin/python3
 """
-Este script se conecta a una base de datos MySQL, ejecuta una consulta para obtener 
-todos los registros de la tabla 'states' ordenados por el campo 'id', e imprime los resultados.
-
-Parámetros:
-    El script acepta tres parámetros desde la línea de comandos:
-        - mysql_user: Nombre de usuario para conectarse a la base de datos MySQL.
-        - mysql_pass: Contraseña del usuario de la base de datos MySQL.
-        - db_name: Nombre de la base de datos a la que se conecta.
-
-Requiere:
-    - MySQLdb (para conectarse a la base de datos MySQL).
-    - sys (para obtener los argumentos de línea de comandos).
-
-Uso:
-    python3 script.py <mysql_user> <mysql_pass> <db_name>
-    Ejemplo:
-        python3 script.py root mypassword mydatabase
+Este script se conecta a una base de datos MySQL
 """
 import MySQLdb
 import sys
