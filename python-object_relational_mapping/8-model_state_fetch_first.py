0d7020cb45ed78c66046b5ddcb67cacd1fde3615
root <root@LAPTOP-4NQDD1B1.>
2025-03-15 22:00:59 -0300

    sa

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+hhijjhjstkks .
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    # Crear la conexión con la base de datos
+    engine = create_engine(
+        f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost/'
+        f'{sys.argv[3]}',
+        pool_pre_ping=True
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+
+    first_state = session.query(State).order_by(State.id).first()
+
+
+    if first_state:
+        print(f"{first_state.id}: {first_state.name}")
+    else:
+        print("Nothing")
+
+
+    session.close()
--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,33 +1,28 @@
 #!/usr/bin/python3
 """
-hhijjhjstkks .
+hhijjhjstkks
 """
-
-
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
+
 if __name__ == "__main__":
 
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
     engine = create_engine(
-    f'mysql+mysqldb://{username}:{password}@localhost/{database}', 
-    pool_pre_ping=True
-)
-
+        f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}',
+        pool_pre_ping=True
+    )
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
 
-    states = session.query(State).order_by(State.id).all()
+    states_with_a = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
 
 
-    for state in states:
+    for state in states_with_a:
         print(f"{state.id}: {state.name}")
 
 
